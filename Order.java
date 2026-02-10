21666b3f402fb928cc0cf864905b805d1af15ebe
Seyha8681 <seyhagaming045@gamil.com>
2026-02-10 21:32:11 +0700

    update

--- Order.java
+++ Order.java
@@ -1,34 +1,45 @@
 import java.util.ArrayList;
 
 public class Order {
-    static int count = 1;
-    int orderId;
-    double grandTotal;
-    ArrayList<OrderProduct> items = new ArrayList<>();
+
+    private static int count = 1;
+    private int orderId;
+    private double grandTotal;
+    private ArrayList<OrderProduct> items;
 
     public Order() {
-        orderId = count++;
+        this.orderId = count++;
+        this.items = new ArrayList<>();
+        this.grandTotal = 0;
+    }
+
+    public static int getTotalOrders() {
+        return count - 1;
     }
 
     public void addItem(OrderProduct item) {
-        items.add(item);
-        grandTotal += item.total;
+        if (item != null) {
+            items.add(item);
+            grandTotal += item.getTotal();
+        }
     }
 
     public void printReceipt() {
-
         System.out.println("\n=========== RECEIPT ==========");
         System.out.println("Order ID: " + orderId);
-
         System.out.println("--------------------------------");
         System.out.println("Product\tQty\tPrice\tTotal");
 
         for (OrderProduct item : items) {
-            System.out.println(item.productName + "\t" +item.quantity + "\t" +item.price + "\t" +item.total
+            System.out.println(item.getProductName() + "\t" +item.getQuantity() + "\t" +item.getPrice() + "\t" +item.getTotal()
             );
         }
 
         System.out.println("--------------------------------");
         System.out.println("Grand Total: $" + grandTotal);
+    }
+
+    public double getGrandTotal() {
+        return grandTotal;
     }
 }
--- OrderProduct.java
+++ OrderProduct.java
@@ -1,13 +1,42 @@
 public class OrderProduct {
-    String productName;
-    int quantity;
-    double price;
-    double total;
+
+    private String productName;
+    private int quantity;
+    private double price;
+    private double total;
 
     public OrderProduct(String productName, int quantity, double price) {
         this.productName = productName;
-        this.quantity = quantity;
-        this.price = price;
-        this.total = quantity * price;
+        setQuantity(quantity);
+        setPrice(price);
+        this.total = this.quantity * this.price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity > 0) {
+            this.quantity = quantity;
+        }
+    }
+
+    public void setPrice(double price) {
+        if (price > 0) {
+            this.price = price;
+        }
     }
 }
